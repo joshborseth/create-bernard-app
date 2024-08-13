c3a2d84b42b64f85d3d7e1c3a9fa6b4dba06e869
joshborseth <joshuaborseth@gmail.com>
2024-08-13 12:31:33 -0700

    rename to create-bernard-app

--- sst.config.ts
+++ sst.config.ts
@@ -1,14 +1,14 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "svelte-tryout",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      home: "aws",
-    };
-  },
-  async run() {
-    new sst.aws.SvelteKit("MyWeb");
-  },
+	app(input) {
+		return {
+			name: 'create-bernard-app',
+			removal: input?.stage === 'production' ? 'retain' : 'remove',
+			home: 'aws'
+		};
+	},
+	async run() {
+		new sst.aws.SvelteKit('MyWeb');
+	}
 });
