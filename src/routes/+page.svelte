5ee26fd1590af83e9f2ac33fcde8efbf610aeb36
joshborseth <joshuaborseth@gmail.com>
2024-08-13 11:55:40 -0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	let count = 0;
+	const increment = () => {
+		count += 1;
+	};
+</script>
+
+<div class="flex flex-col items-center justify-center gap-10 h-screen">
+	<h1 class="text-4xl">Counter: {count}</h1>
+	<button class="bg-blue-400 px-3 py-2 rounded text-white" on:click={increment}>increment</button>
+	{#if count > 10}
+		<p>{count} is greater than 10</p>
+	{:else}
+		<p>{count} is between 0 and 10</p>
+	{/if}
+</div>
